c44086616e5096ec37e6e19c72d7fe70df96e11a
Mathie <math039@outlook.fr>
2024-11-14 18:20:58 +0100

    1*

--- jeu.php
+++ jeu.php
@@ -1,24 +1,37 @@
 <form action="/jeu.php" method="post">
-    <input type="number " name="chiffre" placeholder="Entre 0 et 1000" value="<?= htmlentities($_POST["chiffre"]) ?>">
-    <button type="submit">Deviner</button>
-</form>
+
+    <!-- if faut mettre isset car sinon si la valeur est different d'un number ça fait une erreur -->
+
+
+    <input type="number " name="chiffre" placeholder="Entre 0 et 1000" value="<?php if (isset($_POST["chiffre"]))
+        echo htmlentities($_POST["chiffre"]) ?> ">
+        <button type="submit">Deviner</button>
+
+        <input type="checkbox" name="check" value="fraise">fraise
+        <input type="checkbox" name="check2" value="vanille">vanille
+        <input type="checkbox" name="check3" value="chocolat">chocolat
+
+    </form>
 
 <?php $data = $_POST;
-$Devine = "150";
-
-
-
-
-if (is_int($data["chiffre"])) {
-    echo $data["chiffre"] . " ";
-    if ($data["chiffre"] > $Devine) {
-        echo "Plus bas";
-    } else if ($data["chiffre"] < $Devine) {
-        echo "Plus haut";
-    } else if ($data["chiffre"] === (string) $Devine) {
-        echo "Bravo tu as trouvé le bon numero";
+    $Devine = "150";
+    if ($_POST["check"]) {
+        echo "true";
     }
-} else {
-    echo "ce n'est pas un nombre";
-}
-?>
+
+    echo "cc";
+
+    if (isset($data["chiffre"])) {
+        echo $data["chiffre"] . " ";
+        if ($data["chiffre"] > $Devine) {
+            echo "Plus bas";
+        } else if ($data["chiffre"] < $Devine) {
+            echo "Plus haut";
+        } else if ($data["chiffre"] === (string) $Devine) {
+            echo "Bravo tu as trouvé le bon numero";
+        }
+    } else {
+
+        echo "ce n'est pas un nombre";
+    }
+    ?>
